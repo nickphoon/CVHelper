02be260e3d41c370d94e2ce579c4781f8a20c27d
nickphoon <potatonicholas@gmail.com>
2024-10-17 10:03:24 +0800

    added bash scripts for setting up and running application

---
+++ run.sh
@@ -0,0 +1,3 @@
+source env/Scripts/activate
+
+python app.py
---
+++ setup.sh
@@ -0,0 +1,12 @@
+pip install virtualenv
+
+python -m venv env
+
+source env/Scripts/activate
+
+pip install -r requirements.txt
+
+
+pip list
+
+deactivate
